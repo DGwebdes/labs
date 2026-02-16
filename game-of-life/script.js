90633f9601e744b2cdafa4585d16fb7b5040ad23
DGwebdes <alan.bilik8@gmail.com>
2026-02-16 23:56:59 +0000

    game of life init. will try with js first then some other language

---
+++ game-of-life/script.js
@@ -0,0 +1,9 @@
+/**
+ * * Rules of the Game
+Any live cell with fewer than two live neighbors dies.
+Any live cell with more than three live neighbors dies.
+Any live cell with two or three live neighbors lives, unchanged, to the next generation.
+Any dead cell with exactly three live neighbors will come to life. 
+
+ * TODO setup everything. 
+*/
